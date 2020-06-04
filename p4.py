a4bfca215a34740560254445917af77079c054de
Chotalia <rchotalia@dxc.com>
2020-06-04 14:40:24 +0100

    Another Layer Added. Lyr 2 uses o/p Lyr 1 as i/p

--- p4.py
+++ p4.py
@@ -12,7 +12,16 @@ weights = [[0.2, 0.8, -0.5, 1.0],
            [-0.26, -0.27, 0.17, 0.87]]
 biases = [2, 3, 0.5]
 
+# Second layer
+weights2 = [[0.1, -0.14, 0.5],
+            [-0.5, 0.12, -0.33],
+            [-0.44, 0.73, -0.13]]
+biases2 = [-1, 2, -.5]
+
 # Order of weights and inputs matters because
 # weights is a matrix and inputs is a vector
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
+layer1_output = np.dot(inputs, np.array(weights).T) + biases
+
+layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2
+
+print(layer2_output)
