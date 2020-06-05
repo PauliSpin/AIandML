729fde104a25298914bc38e4f2f5d1e5c24d1136
Chotalia <rchotalia@dxc.com>
2020-06-05 09:02:22 +0100

    Minor Changes

--- p5.py
+++ p5.py
@@ -7,12 +7,14 @@ X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
-# Going to use RELU
+# Going to use ReLU
 # The rectified Linear Activation Unit function
 
 inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
 output = []
 for i in inputs:
+    # The code below can all be replace with
+    # output.append(max(0,i))
     if i > 0:
         output.append(i)
     elif i <= 0:
