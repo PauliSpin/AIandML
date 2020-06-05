3dfcc0614d44486021f76b9bdb959f5a60830783
Chotalia <rchotalia@dxc.com>
2020-06-05 09:36:42 +0100

    Using nnfs

--- p5.py
+++ p5.py
@@ -1,28 +1,20 @@
 import numpy as np
+import nnfs  # See below for details of nnfs
+# https://www.youtube.com/watch?v=gmjzbpSVY1A
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
 
-# Inputs are X; our inout data to the neural network
+
+# X Feature Set
+# y target or classification
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
-# Going to use ReLU
-# The rectified Linear Activation Unit function
+X, y = spiral_data(100, 3)  # 100 Feature sets of 3 classes
 
-inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
-output = []
-for i in inputs:
-    # The code below can all be replace with
-    # output.append(max(0,i))
-    if i > 0:
-        output.append(i)
-    elif i <= 0:
-        output.append(0)
 
-print(output)
-
-'''
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1*np.random.randn(n_inputs, n_neurons)
@@ -32,12 +24,48 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+# layer1 = Layer_Dense(4, 5)  # 4 is the Number of features per sample
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-# print(layer1.output)
 
-layer2.forward(layer1.output)
-print(layer2.output)
-'''
+print(layer1.output)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# NNFS
+# ----
+#
+# pip install nnfs
+# nnfs allows you to get the code from that is being used in the tutorial:
+# Inputs are X; our inout data to the neural network
+# (venv) PS C:\Users\rchotalia\Documents\VisualStudioCode\AIandML> pip install nnfs
+# Collecting nnfs
+#   Downloading nnfs-0.5.0-py3-none-any.whl (9.1 kB)
+# Requirement already satisfied: numpy in c:\users\rchotalia\documents\visualstudiocode\aiandml\venv\lib\site-packages (from nnfs) (1.18.4)
+# Installing collected packages: nnfs
+# Successfully installed nnfs-0.5.0
+# (venv) PS C:\Users\rchotalia\Documents\VisualStudioCode\AIandML> nnfs
+
+# Neural Networks from Scratch in Python Tool.
+
+# Basic usage:
+# nnfs command [parameter1 [parameter2]]
+
+# Detailed usage:
+# nnfs info | code video_part [destination]
+
+# Commands:
+#   info    Prints information about the book
+#   code    Creates a file containing the code of given video part
+#           in given location. Location is optional, example:
+#           nnfs code 2 nnfs/p02.py
+#           will create a file p02.py in a nnfs folder containing
+#           the code of part 2 of video tutorial
+
+# (venv) PS C:\Users\rchotalia\Documents\VisualStudioCode\AIandML>
